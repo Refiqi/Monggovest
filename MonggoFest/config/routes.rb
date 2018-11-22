82c809b9bd5e98d9c2e55e5e9e8da5db0842c981
raufhm <abdulraufmustakim@gmail.com>
2018-11-22 21:50:05 +0700

    fitur auth

--- MonggoFest/config/routes.rb
+++ MonggoFest/config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  
+  post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Home controller routes.
+  # Home controller routes.
+  root   'home#index'
+  get    'auth'            => 'home#auth'
+  
+  # Get login token from Knock
+  post   'user_token'      => 'user_token#create'
+  
+  # User actions
+  get    '/users'          => 'users#index'
+  get    '/users/current'  => 'users#current'
+  post   '/users/create'   => 'users#create'
+  patch  '/user/:id'       => 'users#update'
+  delete '/user/:id'       => 'users#destroy'
+  
 end
